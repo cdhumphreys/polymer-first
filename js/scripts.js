1ca70d2aa4586fcc40d0024535160978fd264aae
Carl Humphreys <chumphreys@opera.com>
2016-03-29 09:46:36 +0100

    Fixed dates, added descending/ascending sort

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,8 @@
 	var chartData = [];
 	var selectedCategory = 'population';
 
+	// +1 for ascending, -1 for descending
+	var sortType = -1;
 
   var sheet = document.querySelector('google-sheets');
   var fetched = false;
@@ -135,19 +137,54 @@
   	currentData.sort(function(a,b){
   		var firstItem = a['gsx$'+category]['$t'];
 		var secondItem = b['gsx$'+category]['$t'];
-		console.log(firstItem);
-		if(moment(firstItem, 'DD/MM/YYYY').format() !== 'Invalid date' && moment(secondItem, 'DD/MM/YYYY').format() !== 'Invalid date') {
-			console.log('we got dates bruv');
+		
+		if(moment(firstItem, 'DD/MM/YYYY', true).format() !== 'Invalid date' && moment(secondItem, 'DD/MM/YYYY', true).format() !== 'Invalid date') {
+			var firstDate = moment(firstItem, 'DD/MM/YYYY', true).format();
+			var secondDate = moment(secondItem, 'DD/MM/YYYY', true).format();
+
+			if (secondDate > firstDate) {
+				if (sortType === -1) {
+					return 1;
+				}
+				else if (sortType === 1) {
+					return -1;
+				}
+				
+			}
+			else if (secondDate === firstDate) {
+				return 0;
+			}
+			else {
+				if (sortType === -1) {
+					return -1;
+				}
+				else if (sortType === 1) {
+					return 1;
+				}
+				
+			}
 		}
 
   		else if (!isNaN(parseInt(firstItem)) && !isNaN(parseInt(secondItem))) { 
   				var firstNum = parseInt(firstItem.replace(/,/g,''));
 				var secondNum = parseInt(secondItem.replace(/,/g,''));
-				return (secondNum - firstNum);
+				if (sortType === -1) {
+					return (secondNum - firstNum);
+				}
+				else if (sortType === 1) {
+					return (firstNum - secondNum);
+				}
+				
   			
   		}
-  		else if (typeof firstItem === 'string' && typeof secondItem === 'string'){ 			
-  			return firstItem.toLowerCase().localeCompare(secondItem.toLowerCase());
+  		else if (typeof firstItem === 'string' && typeof secondItem === 'string'){ 	
+  			if (sortType === -1) {  				
+  				return firstItem.toLowerCase().localeCompare(secondItem.toLowerCase());
+  			}
+  			else if (sortType === 1) {
+  				return -1*(firstItem.toLowerCase().localeCompare(secondItem.toLowerCase()));
+  			}		
+  			
   		}  		
   	});
 
@@ -169,8 +206,16 @@
   			
   			
   });
-  sortBox.addEventListener('change', function(e){
-  		selectedCategory = e.target.name.toLowerCase();   	
+  sortBox.addEventListener('change', function(e){  	
+  		if (e.target.name.toLowerCase() === 'descending') {
+  			sortType = -1;
+  		}
+  		else if (e.target.name.toLowerCase() === 'ascending') {
+  			sortType = 1;
+  		}
+  		else {
+  			selectedCategory = e.target.name.toLowerCase(); 
+  		}  		  	
   		sortData(selectedCategory);
   });
 
